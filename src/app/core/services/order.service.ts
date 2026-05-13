f52a8c5067c3effe4bed2b5e862f902070073f0c
Aman290103 <aman@quickbite.com>
2026-05-13 23:25:57 +0530

    Fix build error: remove unused PlaceOrderDto

--- src/app/core/services/order.service.ts
+++ src/app/core/services/order.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Order, OrderItem, PlaceOrderDto } from '../models';
+import { Order, OrderItem } from '../models';
 import { environment } from '../../../environments/environment';
 
 @Injectable({
